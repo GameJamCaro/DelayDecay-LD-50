b5ad5d9f213f99fb9eda547ee967047a23f9633d
GameJamCaro <79041473+GameJamCaro@users.noreply.github.com>
2022-04-02 09:24:32 +0200

    Initial commit

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Billboard : MonoBehaviour
+{
+
+    public Camera cam;
+    public bool useStaticBillboard;
+
+    private void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (useStaticBillboard)
+        {
+            transform.rotation = cam.transform.rotation;
+        }
+        else 
+        {
+            transform.LookAt(cam.transform);
+        }
+
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+    }
+}
